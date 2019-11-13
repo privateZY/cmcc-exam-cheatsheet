a46442748baf1ea94d1ff110c2374db765752b54
zhangqing <zhangqing332@live.com>
2019-11-13 13:43:48 +0800

    chore: try sftp plugins11

--- getResult.js
+++ getResult.js
@@ -7,9 +7,6 @@ const authHeaders = {
   Authorization: "Bearer__252b25fb62ef7cfd3d33726afbcdfa6d"
 };
 
-
-
-
 axios
   .get(`https://wangda.andedu.net/api/v1/exam/exam/front/exam-paper`, {
     params: {
